dba2026b6e369bf44d3359e295a83d74303cbcca
manishbhurtel <manishbhurtel668@gmail.com>
2022-07-14 22:23:44 +0545

    sdgdg

--- src/Components/Body2.js
+++ src/Components/Body2.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import '../Components/CSS/Body2.css'
+import bImg from '../Images/plate.png'
 
 function Body2() {
-  return (
-    <>
-     <div id='Body2_container'>
-        Hello this is Body 2
-     </div>
-    </>
-  )
+    return (
+        <>
+            <div id='Body2_container'>
+                <img src={bImg} alt="body2img" className='body2img' />
+                <div className="textarea_container">
+                <div className="textarea">
+                    <div className="b2header">
+                       <b> Clean Process</b>
+                    </div>
+                    <div className="b2text">
+                        We are living in the world where foods are creating values in the market and to fulfill the demands of the public, we make delicious food with maintaining cleanliness throughout the process.
+                    </div>
+                    <div className="body2_button">
+                        <a href='#'><button>See more</button></a>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Body2
